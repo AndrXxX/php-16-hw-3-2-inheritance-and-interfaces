86af119dd6fe077ece8820166ed201576435af7c
Unknown <andrew1gar@yandex.ru>
2017-10-22 19:21:25 +0300

    ver 0.1

---
+++ main.php
@@ -0,0 +1,371 @@
+<?php
+
+interface BasicInfo
+{
+    public function getFullDescription($itemType = true);
+}
+
+class Item implements BasicInfo
+{
+    protected $name;
+    protected $cost;
+    protected $color;
+
+    public function __construct($name, $cost, $color)
+    {
+        $this->name = $name;
+        $this->cost = $cost;
+        $this->color = $color;
+    }
+
+    public function getFullDescription($itemType = true)
+    {
+        $format = "%s цвета %s, со стоимостью %u руб.";
+        $format = ($itemType) ? 'Предмет ' . $format : $format;
+        return sprintf($format, $this->getName(), $this->getColor(), $this->getCost());
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    public function getCost()
+    {
+        return $this->cost;
+    }
+
+    public function setCost($cost)
+    {
+        $this->cost = $cost;
+    }
+}
+
+class Car extends Item
+{
+    protected $color = 'white';
+
+    public function getFullDescription($itemType = true)
+    {
+        return ' Машина ' . parent::getFullDescription(false);
+    }
+}
+
+class TelevisionSet extends Item
+{
+    const STATE_OFF = 'выключен';
+    const STATE_ON = 'включен';
+
+    private $state = self::STATE_OFF;
+    private $channel = 1;
+    private $volume = 50;
+
+    public function volumeUp()
+    {
+        $this->volume = ($this->state === self::STATE_ON) && ($this->volume) < 100 ? ++$this->volume : $this->volume;
+    }
+
+    public function volumeDown()
+    {
+        $this->volume = ($this->state === self::STATE_ON) && ($this->volume) > 0 ? --$this->volume : $this->volume;
+    }
+
+    public function turnOn()
+    {
+        $this->state = self::STATE_ON;
+    }
+
+    public function turnOff()
+    {
+        $this->state = self::STATE_OFF;
+    }
+
+    public function getFullDescription($itemType = true)
+    {
+        $format = " Телевизор сейчас %s, его громкость %s, канал №%u.<br>";
+        return ' Телевизор ' . parent::getFullDescription(false) .
+            sprintf($format, $this->getState(), $this->getVolume(), $this->getChannel());
+    }
+
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    public function getVolume()
+    {
+        return $this->volume;
+    }
+
+    public function getChannel()
+    {
+        return $this->channel;
+    }
+
+    public function setChannel($channel)
+    {
+        $this->channel = $this->state === self::STATE_ON ? $channel : $this->channel;
+    }
+}
+
+class Pen extends Item
+{
+    private $thickness;
+    private $inkLevel;
+
+    public function usePen()
+    {
+        $this->inkLevel = $this->inkLevel > 0 ? --$this->inkLevel : 0;
+    }
+
+    public function getFullDescription($itemType = true)
+    {
+        $format = " У этой ручки толщина стержня %01.1f, уровень чернил %u.<br>";
+        return ' Ручка ' . parent::getFullDescription(false) .
+            sprintf($format, $this->getThickness(), $this->getInkLevel());
+    }
+
+    public function getThickness()
+    {
+        return $this->thickness;
+    }
+
+    public function setThickness($thickness)
+    {
+        $this->thickness = $thickness;
+    }
+
+    public function getInkLevel()
+    {
+        return $this->inkLevel;
+    }
+
+    public function setInkLevel($inkLevel = 100)
+    {
+        $this->inkLevel = $inkLevel;
+    }
+}
+
+class Duck extends Item
+{
+    private $age;
+    private $weight;
+
+    public function changeWeight($changeWeight)
+    {
+        $this->weight = ($this->weight + $changeWeight) >= 1 ? ($this->weight + $changeWeight) : 1;
+    }
+
+    public function upAge()
+    {
+        $this->age = ++$this->age;
+    }
+
+    public function getFullDescription($itemType = true)
+    {
+        $format = "Возраст этой утки %u, ее вес %u кг.<br>";
+        return ' Утка ' . parent::getFullDescription(false) .
+            sprintf($format, $this->getAge(), $this->getWeight());
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+    }
+}
+
+class Product extends Item
+{
+    private static $groupDiscount = [];
+    private $price;
+    private $productDiscount;
+    private $productGroup;
+
+    public function getFullDescription($itemType = true)
+    {
+        $format = "Товар %1\$s %2\$s цвета %3\s цена %4\$u руб., скидка на всю группу %1\$s - %5\$u, 
+          скидка именно на %1\$s %2\$s - %6\$u. <br>";
+        return vsprintf($format, $this->getProductInfo());
+    }
+
+    public function getProductInfo()
+    {
+        $product = [
+            $this->getProductGroup(),
+            $this->getName(),
+            $this->getColor(),
+            $this->getPrice(),
+            self::getGroupDiscount($this->productGroup),
+            $this->getProductDiscount()
+        ];
+        return $product;
+    }
+
+    public function getProductGroup()
+    {
+        return isset($this->productGroup) ? $this->productGroup : '';
+    }
+
+    public function setProductGroup($productGroup)
+    {
+        $this->productGroup = $productGroup;
+    }
+
+    /**
+     * выводит цену с учетом скидки (выбирается максимальная скидка)
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        $discount = max($this->productDiscount, self::getGroupDiscount($this->productGroup));
+        return $this->price * (1 + $discount / 100);
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public static function getGroupDiscount($group)
+    {
+        return isset(self::$groupDiscount[$group]) ? self::$groupDiscount[$group] : 0;
+    }
+
+    public static function setGroupDiscount($group, $discount)
+    {
+        self::$groupDiscount[$group] = $discount;
+    }
+
+    public function getProductDiscount()
+    {
+        return isset($this->productDiscount) ? $this->productDiscount : 0;
+    }
+
+    public function setProductDiscount($productDiscount)
+    {
+        $this->productDiscount = $productDiscount;
+    }
+}
+
+/* ----------- */
+
+$carAudi = new Car('Audi', 1000000, 'white');
+$carKia = new Car('Kia', 800000, 'black');
+
+$formatCar = "Машина: %s цвета %s и стоимостью %u руб.<br>";
+
+echo sprintf($formatCar, $carAudi->getName(), $carAudi->getColor(), $carAudi->getCost());
+echo sprintf($formatCar, $carKia->getName(), $carKia->getColor(), $carKia->getCost());
+echo '<br>';
+
+/* ----------- */
+
+$tvPhilips = new TelevisionSet('Philips', 10000, 'black');
+$tvSony = new TelevisionSet('Sony', 15000, 'black');
+
+$tvPhilips->turnOn();
+$tvPhilips->setChannel(10); // канал изменится, т.к. телевизор включен
+$tvSony->setChannel(10); // канал не изменится, т.к. телевизор выключен
+
+for ($i = 1; $i < 10; $i++) {
+    $tvPhilips->volumeUp(); // громкость меняться будет, т.к. телевизор включен
+    $tvSony->volumeDown(); // громкость меняться не будет, т.к. телевизор выключен
+
+}
+$tvPhilips->turnOff();
+$tvSony->turnOn();
+
+echo $tvPhilips->getFullDescription();
+echo $tvSony->getFullDescription();
+echo '<br>';
+
+/* ----------- */
+
+$whitePen = new Pen('Pen1', 10, 'white');
+$whitePen->setInkLevel(80);
+$whitePen->setThickness('0.5');
+$blackPen = new Pen('Pen2', 20, 'black');
+$whitePen->setInkLevel();
+$whitePen->setThickness('0.7');
+
+for ($i = 0; $i < 150; $i++) {
+    if (rand(1, 2) === 1) {
+        $whitePen->usePen();
+    } else {
+        $blackPen->usePen();
+    }
+}
+
+echo $whitePen->getFullDescription();
+echo $blackPen->getFullDescription();
+echo '<br>';
+
+/* ----------- */
+
+$brownDuck = new Duck('FirstDuck', 500, 'brown');
+$brownDuck->setWeight(2);
+$brownDuck->setAge(2);
+$blackDuck = new Duck('SecondDuck', 600, 'black');
+
+for ($i = 0; $i < 10; $i++) {
+    $brownDuck->changeWeight(rand(-1, 1));
+    $blackDuck->changeWeight(rand(-1, 1));
+    $brownDuck->upAge();
+    $blackDuck->upAge();
+}
+
+echo $brownDuck->getFullDescription();
+echo $blackDuck->getFullDescription();
+echo '<br>';
+
+/* ----------- */
+
+$phoneSamsung = new Product('Samsung', 20000, 'Черный');
+$phoneSamsung->setProductGroup('Смартфон');
+
+$phoneApple = new Product('Apple', 30000, 'Белый');
+$phoneApple->setProductDiscount(10);
+$phoneApple->setProductGroup('Смартфон');
+
+$caseSamsung = new Product('Samsung', 600, 'Черный');
+$caseSamsung->setProductDiscount(15);
+$caseSamsung->setProductGroup('Чехол');
+
+$caseApple = new Product('Apple', 700, 'Белый');
+$caseApple->setProductDiscount(25);
+$caseApple->setProductGroup('Чехол');
+
+Product::setGroupDiscount('Смартфон', 5);
+Product::setGroupDiscount('Чехол', 20);
+
+echo $phoneSamsung->getFullDescription();
+echo $phoneApple->getFullDescription();
+echo $caseSamsung->getFullDescription();
+echo $caseApple->getFullDescription();
+echo '<br>';
+
---
+++ product.php
@@ -0,0 +1,188 @@
+<?php
+
+interface PrintDescription
+{
+    public function getName();
+
+    public function getCost();
+
+    public function getWeight();
+
+    public function getProductDiscount();
+
+    public function getAdvancedDescription();
+
+    public function getDeliveryCost();
+}
+
+abstract class Product implements PrintDescription
+{
+    protected $name;
+    protected $cost;
+    protected $productDiscount = 10;
+    protected $weight;
+
+    public function __construct($name, $cost)
+    {
+        $this->name = $name;
+        $this->cost = $cost;
+    }
+
+    public function getName()
+    {
+        return isset($this->name) ? $this->name : '';
+    }
+
+    public function getCost()
+    {
+        return $this->cost * (1 + ($this->getProductDiscount() / 100));
+    }
+
+    public function setCost($cost)
+    {
+        $this->cost = $cost;
+    }
+
+    public function getProductDiscount()
+    {
+        return isset($this->productDiscount) ? $this->productDiscount : 0;
+    }
+
+    public function setProductDiscount($productDiscount)
+    {
+        $this->productDiscount = $productDiscount;
+    }
+
+    public function getWeight()
+    {
+        return isset($this->weight) ? $this->weight : 0;
+    }
+
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+    }
+
+    /**
+     * Доставка на все продукты = 250 рублей, но если на продукт была скидка, то 300 рублей.
+     * @return int
+     */
+    public function getDeliveryCost()
+    {
+        return $this->getProductDiscount() > 0 ? 300 : 250;
+    }
+
+    abstract public function getAdvancedDescription();
+}
+
+final class Phone extends Product
+{
+    private $screen;
+
+    public function getAdvancedDescription()
+    {
+        return 'Размер экрана: ' . $this->getScreen() . ' дюймов.';
+    }
+
+    public function getScreen()
+    {
+        return $this->screen;
+    }
+
+    public function setScreen($screen)
+    {
+        $this->screen = $screen;
+    }
+}
+
+final class Router extends Product
+{
+    protected $productDiscount = 0;
+    private $speed;
+
+    public function getAdvancedDescription()
+    {
+        return 'Скорость: ' . $this->getSpeed() . ' Мбит/с.';
+    }
+
+    public function getSpeed()
+    {
+        return $this->speed;
+    }
+
+    public function setSpeed($speed)
+    {
+        $this->speed = $speed;
+    }
+}
+
+final class Telescope extends Product
+{
+    private $zoom;
+
+    public function getAdvancedDescription()
+    {
+        return 'Кратность увеличения: ' . $this->getZoom() . '.';
+    }
+
+    public function getZoom()
+    {
+        return $this->zoom;
+    }
+
+    public function setZoom($zoom)
+    {
+        $this->zoom = $zoom;
+    }
+
+    /**
+     * Переопределяем вывод скидки - продукт имеет скидку только в том случае, если его вес больше 10 килограмм.
+     * @return int
+     */
+    public function getProductDiscount()
+    {
+        return (isset($this->productDiscount)) && ($this->getWeight() > 10) ? $this->productDiscount : 0;
+    }
+}
+
+function getObjectDescription(PrintDescription $obj)
+{
+    $basicDescr = "Товар %s, его цена со скидкой %u руб., скидка %u процентов, вес %s кг. ";
+    $advancedDescr = "Дополнительная информация: %s ";
+    $deliveryDescr = "Стоимость доставки: %u руб.";
+
+    return sprintf($basicDescr, $obj->getName(), $obj->getCost(), $obj->getProductDiscount(), $obj->getWeight()) .
+        sprintf($advancedDescr, $obj->getAdvancedDescription()) .
+        sprintf($deliveryDescr, $obj->getDeliveryCost());
+}
+
+$phone1 = new Phone('Samsung J1', 15000);
+$phone1->setWeight(0.5);
+$phone1->setScreen(5);
+echo getObjectDescription($phone1) . '<br>';
+
+$phone2 = new Phone('Apple iPhone 5s', 25000);
+$phone2->setWeight(0.6);
+$phone2->setScreen(4.5);
+echo getObjectDescription($phone2) . '<br><br>';
+
+$router1 = new Router('Zyxel', 1500);
+$router1->setWeight(0.2);
+$router1->setSpeed(1000);
+echo getObjectDescription($router1) . '<br>';
+
+$router2 = new Router('TP-Link', 1200);
+$router2->setWeight(0.2);
+$router2->setSpeed(100);
+echo getObjectDescription($router2) . '<br><br>';
+
+$telescope1 = new Telescope('telescope1', 20000);
+$telescope1->setWeight(9);
+$telescope1->setZoom(2);
+echo getObjectDescription($telescope1) . '<br>';
+
+$telescope2 = new Telescope('telescope1', 20000);
+$telescope2->setWeight(11);
+$telescope2->setZoom(5);
+echo getObjectDescription($telescope2) . '<br><br>';
+
